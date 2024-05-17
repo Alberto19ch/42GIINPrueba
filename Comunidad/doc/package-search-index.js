d5d5fda5b0516d9151b0a2fb93ffb2d64e0e44ab
Alberto Granado <albertknig9@gmail.com>
2024-05-17 04:10:43 +0200

    eliminado doc

--- Comunidad/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"conceptos"},{"l":"gastos"},{"l":"inmuebles"},{"l":"liquidaciones"},{"l":"servicios"}];updateSearchResults();
